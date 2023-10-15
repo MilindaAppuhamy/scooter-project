2d2fca3c1fdae81644aabceaa90123bd1719feb9
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-10-15 21:46:09 +0100

    User tests and User class created.

--- src/User.js
+++ src/User.js
@@ -1,5 +1,24 @@
 class User {
-  // User code here
+  constructor(username, password, age) {
+    this.username = username;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = false;
+  }
+
+  login(password) {
+    if (password === this.password) {
+      this.loggedIn = true;
+      console.log("User logged in.");
+    } else {
+      throw new Error("Incorrect password.");
+    }
+  }
+
+  logout() {
+    this.loggedIn = false;
+    console.log("User logged out.");
+  }
 }
 
-module.exports = User
+module.exports = User;
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,19 +1,54 @@
-const User = require('../src/User');
+const User = require("../src/User");
 
 const user = new User("Joe Bloggs", "test123", 21);
 
-// User tests here
 describe("User property tests", () => {
   // test username
   test("username should be a string", () => {
     expect(typeof user.username).toBe("string");
-  })
+  });
   // test password
+  test("password should be a string", () => {
+    expect(typeof user.password).toBe("string");
+  });
 
   // test age
-})
+  test("age should be a number", () => {
+    expect(typeof user.age).toBe("number");
+  });
 
+  //object
+  test("user is an instance of User", () => {
+    expect(user instanceof User).toBeTruthy();
+  });
+
+  test("user should create a valid user object", () => {
+    expect(user).toEqual({
+      username: "Joe Bloggs",
+      password: "test123",
+      age: 21,
+      loggedIn: false,
+    });
+  });
+});
 
 // test login
+describe("Login method", () => {
+  it("should throw an error if the password is incorrect", () => {
+    expect(() => user.login("1234")).toThrow("Incorrect password.");
+  });
+
+  it("should log the user properly if the password is correct", () => {
+    user.login(user.password);
+    expect(user.loggedIn).toBeTruthy();
+  });
+});
 
 // test logout
+describe("Logout method", () => {
+  it("should logout the user properly", () => {
+    user.login(user.password);
+    user.logout();
+    expect(user.loggedIn).toBeFalsy();
+  });
+});
